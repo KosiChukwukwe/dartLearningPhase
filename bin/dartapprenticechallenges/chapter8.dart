571b72f91722405902a99c659e3e822fd035e985
KosiChukwukwe <deborahchukwukwe@yahoo.com>
2022-07-12 15:10:29 +0100

    more answers to questions in chapter 8

--- bin/dartapprenticechallenges/chapter8.dart
+++ bin/dartapprenticechallenges/chapter8.dart
@@ -48,10 +48,18 @@ void main() {
 
   //Answer:
 
+  var capitalisedMonths = [
+    for (var element in months) element.toUpperCase(),
+  ];
+  print(capitalisedMonths);
+
+  //or
+
   // ignore: avoid_function_literals_in_foreach_calls
-  months.forEach((element) {
-    print(element.toUpperCase());
-  });
+
+  // months.forEach((element) {
+  //   print(element.toUpperCase());
+  // });
 
   //or
 
@@ -64,7 +72,50 @@ void main() {
    */
 
   final anotherSet = {1, 2, 3, 1};
-  print(anotherSet);
+  print(anotherSet); //even though there are two 1s, it prints just one
+
+  /**
+   * Practise for the section on maps
+   */
 
   //Second Mini Challenge:
+
+  //QUESTION ONE
+
+  /**
+   * Create a map with the following keys: 
+   * name, profession, country and city. 
+   * For the values, add your own information.
+   */
+
+  //Answer:
+
+  final details = <String, String>{
+    'name': 'Kosi',
+    'profession': 'Student',
+    'country': 'Nigeria',
+    'city': 'Awka',
+  };
+
+  //QUESTION TWO
+
+  /**
+   * You suddenly decide to move to Toronto, Canada. 
+   * Programmatically update the values for country and city.
+   */
+
+  //Answer:
+
+  details['country'] = 'Canada';
+  details['city'] = 'Toronto';
+
+  //QUESTION THREE
+
+  //Iterate over the map and print all the values.
+
+  //Answer:
+
+  details.forEach((key, value) {
+    print('$key - $value');
+  });
 }
